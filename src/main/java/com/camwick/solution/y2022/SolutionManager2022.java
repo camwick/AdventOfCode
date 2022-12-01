73b1726420397b6facfc623901d65c39255e9cd0
camwick <cwickers@pnw.edu>
2022-12-01 13:30:43 -0600

    prep for 12/2/2022

--- src/main/java/com/camwick/solution/y2022/SolutionManager2022.java
+++ src/main/java/com/camwick/solution/y2022/SolutionManager2022.java
@@ -6,6 +6,7 @@ import org.apache.http.client.ClientProtocolException;
 
 import com.camwick.solution.SolutionManager;
 import com.camwick.solution.y2022.day1.*;
+import com.camwick.solution.y2022.day2.Day2Part1;
 
 public class SolutionManager2022 extends SolutionManager{
     public SolutionManager2022(boolean test, String fileName) throws ClientProtocolException, IOException{
@@ -14,5 +15,6 @@ public class SolutionManager2022 extends SolutionManager{
         // add solutions to manager here...
         this.solutions.add(new Day1Part1(test, fileName));
         this.solutions.add(new Day1Part2(test, fileName));
+        this.solutions.add(new Day2Part1(test, fileName));
     }
 }
---
+++ src/main/java/com/camwick/solution/y2022/day2/Day2Part1.java
@@ -0,0 +1,19 @@
+package com.camwick.solution.y2022.day2;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+
+import com.camwick.solution.ProblemSolution;
+
+public class Day2Part1 extends ProblemSolution{
+
+    public Day2Part1(boolean test, String fileName) throws ClientProtocolException, IOException {
+        super("2022", "2", "1", test, fileName);
+    }
+
+    @Override
+    public void solve() {
+        
+    }
+}
